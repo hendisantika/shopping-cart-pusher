e328d14cf0d76aa70d3a24b2d30d19d0effbc071
hendisantika <hendisantika@yahoo.co.id>
2024-01-31 09:16:33 +0700

    Add PusherConstants ✏️📗 :octocat:⬆

---
+++ src/main/java/id/my/hendisantika/shoppingcartpusher/constants/PusherConstants.java
@@ -0,0 +1,35 @@
+package id.my.hendisantika.shoppingcartpusher.constants;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : shopping-cart-pusher
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/31/24
+ * Time: 09:16
+ * To change this template use File | Settings | File Templates.
+ */
+public interface PusherConstants {
+    /**
+     * Pusher App ID
+     */
+    String PUSHER_APP_ID = "278408";
+
+    /**
+     * Pusher App Key
+     */
+    String PUSHER_APP_KEY = "86e2eec9042d222b8eb5";
+
+    /**
+     * Pusher App Secret
+     */
+    String PUSHER_APP_SECRET = "f80ce0363567465627bc";
+
+    String PUSHER_CLUSTER_KEY = "eu";
+
+    /**
+     * Pusher channel name where the events will be published
+     */
+    String CHANNEL_NAME = "shopping-cart";
+}
