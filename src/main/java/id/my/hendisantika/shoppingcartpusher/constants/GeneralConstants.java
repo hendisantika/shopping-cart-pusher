9ad61accab21fbfcea5d28941bc90ee9db266b2b
hendisantika <hendisantika@yahoo.co.id>
2024-01-31 09:16:03 +0700

    Add GeneralConstants ✏️📗 :octocat:⬆

---
+++ src/main/java/id/my/hendisantika/shoppingcartpusher/constants/GeneralConstants.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.shoppingcartpusher.constants;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : shopping-cart-pusher
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/31/24
+ * Time: 09:15
+ * To change this template use File | Settings | File Templates.
+ */
+public interface GeneralConstants {
+    /**
+     * ID in the session of the shopping cart object
+     */
+    String ID_SESSION_SHOPPING_CART = "shopping-cart-1";
+}
