28f2939495e7d3ba5e6fbb6234e439988a49afe1
hendisantika <hendisantika@yahoo.co.id>
2024-01-31 09:04:16 +0700

    Create starter project ✏️📗 :octocat:⬆

---
+++ src/main/java/id/my/hendisantika/shoppingcartpusher/ShoppingCartPusherApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.shoppingcartpusher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartPusherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingCartPusherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/shoppingcartpusher/ShoppingCartPusherApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.shoppingcartpusher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartPusherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
